0a24fc0d46fceff2b277de56c5b5e1644e8c8511
go-suu <67548233+go-suu@users.noreply.github.com>
2020-08-04 02:23:00 +0900

    db에 저장안함으로 바꿈

--- movie_scraping.py
+++ movie_scraping.py
@@ -1,9 +1,3 @@
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 먼저 해야겠죠?)
-
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta
-
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -19,8 +13,6 @@ for movie in movies:
     title = movie.select_one("td.title > div > a")
     star = movie.select_one("td.point")
     rank = movie.select_one('td:nth-child(1) > img')
+
     if title is not None:
-
-        doc = {"rank" : rank['alt'], "title" : title.text,"star" : star.text}
-
-        db.movies.insert_one(doc)
+        print(rank['alt'], title.text ,star.text)
